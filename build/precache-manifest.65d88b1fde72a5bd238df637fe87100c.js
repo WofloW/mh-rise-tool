cfaa62bcbabfde6bc57afe9b99c350ebbdd25fba
weifang <lancefangw@gmail.com>
2021-04-19 11:54:07 -0700

    first build

---
+++ build/precache-manifest.65d88b1fde72a5bd238df637fe87100c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7182a099e8ed9da9d9b39a5f6afdd216",
+    "url": "/salmonrun-rotation-static/index.html"
+  },
+  {
+    "revision": "8fc835f05f4cf8968cab",
+    "url": "/salmonrun-rotation-static/static/css/main.634b3087.chunk.css"
+  },
+  {
+    "revision": "8e5664b786e54f9d30d3",
+    "url": "/salmonrun-rotation-static/static/js/2.2129da82.chunk.js"
+  },
+  {
+    "revision": "8fc835f05f4cf8968cab",
+    "url": "/salmonrun-rotation-static/static/js/main.0614738c.chunk.js"
+  },
+  {
+    "revision": "7df4892acf3580c56643",
+    "url": "/salmonrun-rotation-static/static/js/runtime~main.115a109d.js"
+  }
+]);
