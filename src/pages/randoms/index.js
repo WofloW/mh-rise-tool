20061b6e9ee90f744463b59db118584ca182bde9
weifang <lancefangw@gmail.com>
2021-04-19 20:33:23 -0700

    add random cats

--- src/pages/randoms/index.js
+++ src/pages/randoms/index.js
@@ -2,8 +2,6 @@ import React, {useState} from 'react'
 import weapons from '../../data/weapons'
 import monsters from '../../data/monsters'
 import _ from 'lodash'
-import cat from '../../images/cat.png'
-import cat2 from '../../images/cat2.jpg'
 
 const randomWeapon = () => {
   return weapons[_.random(0, 13)]
@@ -12,10 +10,6 @@ const randomWeapon = () => {
 export default function Randoms() {
   const [random, setRandom] = useState(null)
   return <div style={{maxWidth: 500, margin: 'auto', padding: 30}}>
-    <div>
-      <img src={cat} className='cat-image'/>
-      <img src={cat2} className='cat-image'/>
-    </div>
     <button className='random-button' style={{marginTop: 30}} onClick={() => {
       setRandom(Math.random())
     }}>重新随机
@@ -28,5 +22,9 @@ export default function Randoms() {
     }
     <h3 style={{marginTop: 30}}>随机怪物</h3>
     <div>{monsters[_.random(0, 21)]}</div>
+    <div>
+      <h3>随机猫猫</h3>
+      <img src={`/images/cats/${_.random(1, 25)}.jpg`}/>
+    </div>
   </div>
 }
